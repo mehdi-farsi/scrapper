3b535677e870236c09622f9d951b6082eee0e1ae
Mehdi FARSI <mehdifarsi.pro@gmail.com>
2015-12-14 08:53:15 +0100

    fix(datatables): limit 200

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @startups = Startup.includes(:founders, :social_networks).all
+    @startups = Startup.includes(:founders, :social_networks).limit(200).all
   end
 end
